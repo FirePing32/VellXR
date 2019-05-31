efa95b2b99b92ac9021f327bc9a5f9ddbd87c0b6
Prakhar Gurunani <prakhargurunani@gmail.com>
2019-05-31 21:50:18 +0530

    Temp fix for ckeditor

--- vellxr/settings.py
+++ vellxr/settings.py
@@ -195,6 +195,8 @@ logging.config.dictConfig({
     }
 })
 
+CKEDITOR_JQUERY_URL = 'https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js'
+
 CKEDITOR_CONFIGS = {
    'default': {
        'toolbar_Full': [
