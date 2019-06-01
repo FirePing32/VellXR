e88e6a59756fc0c5f7491545b2916b3221838869
Prakhar Gurunani <prakhargurunani@gmail.com>
2019-06-01 12:25:28 +0530

    Changed CKeditor settings

--- user/models.py
+++ user/models.py
@@ -35,7 +35,7 @@ class Post(models.Model):
     slug = models.SlugField(max_length=50)
     title = models.CharField(max_length=50) 
     post_image = CloudinaryField('image', blank=True, null=True, default="logo.png")
-    content = models.CharField() 
+    content = models.TextField() 
     published_date = models.DateTimeField(default=timezone.now) 
 
     def publish(self): 
