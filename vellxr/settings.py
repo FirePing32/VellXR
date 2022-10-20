f5baeea315dce3e0a761e7991261ed5d25abae4c
Prakhar Gurunani <prakhargurunani@gmail.com>
2022-10-20 16:33:11 +0530

    Drop SQLite 3

--- vellxr/settings.py
+++ vellxr/settings.py
@@ -104,12 +104,12 @@ WSGI_APPLICATION = 'vellxr.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 
 # Password validation
